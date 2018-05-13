37cca6a3212fd8c5ec04ea63211193b33ea1fd83
blakeca00 <blakeca00@gmail.com>
2018-05-13 12:54:01 -0500

    fix init modules

--- utaws/ec2/__init__.py
+++ utaws/ec2/__init__.py
@@ -1,6 +1,3 @@
-
-__author__ = 'Blake Huber'
-__license__ = "MIT"
-__maintainer__ = "Blake Huber"
-__email__ = "blakeca00[AT]gmail.com"
-__status__ = "Development"
+"""
+Functionality utilising Amazon EC2 Service
+"""
