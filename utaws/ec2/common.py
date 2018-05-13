3334e6e5a17368c3700a1a9415569526e97259f3
blakeca00 <blakeca00@gmail.com>
2018-05-13 11:09:40 -0500

    repo init

---
+++ utaws/ec2/common.py
@@ -0,0 +1,72 @@
+import os
+import inspect
+import boto3
+from botocore.exceptions import ClientError
+
+
+def authenticated(profile):
+    """
+    Summary:
+        Tests generic authentication status to AWS Account
+    Args:
+        :profile (str): iam user name from local awscli configuration
+    Returns:
+        TYPE: bool, True (Authenticated)| False (Unauthenticated)
+    """
+    try:
+        sts_client = boto3_session(service='sts', profile=profile)
+        httpstatus = sts_client.get_caller_identity()['ResponseMetadata']['HTTPStatusCode']
+        if httpstatus == 200:
+            return True
+
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'InvalidClientTokenId':
+            logger.info(
+                '%s: Invalid credentials to authenticate for profile user (%s). Exit. [Code: %d]'
+                % (inspect.stack()[0][3], profile, exit_codes['EX_NOPERM']['Code']))
+        elif e.response['Error']['Code'] == 'ExpiredToken':
+            logger.info(
+                '%s: Expired temporary credentials detected for profile user (%s) [Code: %d]'
+                % (inspect.stack()[0][3], profile, exit_codes['EX_CONFIG']['Code']))
+        else:
+            logger.exception(
+                '%s: Unknown Boto3 problem. Error: %s' %
+                (inspect.stack()[0][3], e.response['Error']['Message']))
+    except Exception as e:
+        return False
+    return False
+
+
+def boto3_session(service, profile=None):
+    """
+    Summary:
+        Establishes boto3 sessions, client
+    Args:
+        :service (str): boto3 service abbreviation ('ec2', 's3', etc)
+        :profile (str): profile_name of an iam user from local awscli config
+    Returns:
+        TYPE: boto3 client object
+    """
+    try:
+        if profile:
+            if profile == 'default':
+                client = boto3.client(service)
+            else:
+                session = boto3.Session(profile_name=profile)
+                client = session.client(service)
+        else:
+            client = boto3.client(service)
+    except ClientError as e:
+        logger.exception(
+            "%s: IAM user or role not found (Code: %s Message: %s)" %
+            (inspect.stack()[0][3], e.response['Error']['Code'],
+             e.response['Error']['Message']))
+        raise
+    except ProfileNotFound:
+        msg = (
+            '%s: The profile (%s) was not found in your local config. Exit.' %
+            (inspect.stack()[0][3], profile))
+        stdout_message(msg, 'FAIL')
+        logger.warning(msg)
+        sys.exit(exit_codes['EX_NOUSER']['Code'])
+    return client
---
+++ utaws/ec2/latest-amis.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+import json
+import inspect
+import logging
+import boto3
+from botocore.exceptions import ClientError
+from .common import authenticated
+from .script_utils import stdout_message
+
+try:
+    from .oscodes_unix import exit_codes
+except Exception:
+    from .oscodes_win import exit_codes    # non-specific os-safe codes
+
+# globals
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+session = boto3.Session(profile_name='gcreds-da-atos')
+
+
+def get_regions():
+    """ Return list of all regions """
+    client = session.client('ec2')
+    return [x['RegionName'] for x in client.describe_regions()['Regions']]
+
+
+def amazonlinux(profile=None):
+    """
+    Return latest current amazonlinux v1 AMI for each region
+    """
+    latest = {}
+    for region in regions:
+        try:
+            client = session.client('ec2', region_name=region)
+            r = client.describe_images(
+                Owners=['amazon'],
+                Filters=[
+                    {
+                        'Name': 'name',
+                        'Values': [
+                            'amzn-ami-hvm-2018.??.?.2018????-x86_64-gp2'
+                        ]
+                    }
+                ])
+            latest[region] = r['Images']
+        except ClientError as e:
+            logger.exception(
+                '%s: Boto error while retrieving AMI data (%s)' %
+                (inspect.stack()[0][3], str(e)))
+            continue
+        except Exception as e:
+            logger.exception(
+                '%s: Unknown Exception occured while retrieving AMI data (%s)' %
+                (inspect.stack()[0][3], str(e)))
+            raise e
+    return latest
+
+
+def amazonlinux2(profile=None):
+    """
+    Return latest current amazonlinux v1 AMI for each region
+    """
+    latest = {}
+    for region in regions:
+        try:
+            client = session.client('ec2', region_name=region)
+            r = client.describe_images(
+                Owners=['amazon'],
+                Filters=[
+                    {
+                        'Name': 'name',
+                        'Values': [
+                            'amzn2-ami-hvm-????.??.?.2018????.?-x86_64-gp2',
+                            'amzn2-ami-hvm-????.??.?.2018????-x86_64-gp2'
+                        ]
+                    }
+                ])
+            latest[region] = r['Images']
+        except ClientError as e:
+            logger.exception(
+                '%s: Boto error while retrieving AMI data (%s)' %
+                (inspect.stack()[0][3], str(e)))
+            continue
+        except Exception as e:
+            logger.exception(
+                '%s: Unknown Exception occured while retrieving AMI data (%s)' %
+                (inspect.stack()[0][3], str(e)))
+            raise e
+    return latest
+
+
+def options(parser, help_menu=True):
+    """
+    Summary:
+        parse cli parameter options
+    Returns:
+        TYPE: argparse object, parser argument set
+    """
+    parser.add_argument("-p", "--profile", nargs='?', default="default",
+    parser.add_argument("-V", "--version", dest='version', action='store_true', required=False)
+    #parser.add_argument("-h", "--help", dest='help', action='store_true', required=False)
+    return parser.parse_args()
+
+
+def init_cli():
+    # parser = argparse.ArgumentParser(add_help=False, usage=help_menu())
+    parser = argparse.ArgumentParser(add_help=True)
+
+    try:
+        args = options(parser)
+    except Exception as e:
+        #help_menu()
+        stdout_message(str(e), 'ERROR')
+        sys.exit(exit_codes['EX_OK']['Code'])
+
+    if len(sys.argv) == 1:
+        #help_menu()
+        sys.exit(exit_codes['EX_OK']['Code'])
+
+    elif authenticated(profile=args.profile):
+        # execute keyset operation
+        success = main(
+                    operation=args.operation,
+                    profile=args.profile,
+                    user_name=args.username,
+                    auto=args.auto,
+                    debug=args.debug
+                    )
+        if success:
+            logger.info('Job complete')
+            sys.exit(exit_codes['EX_OK']['Code'])
+    else:
+        stdout_message(
+            'Authenication Failed to AWS Account for user %s' % args.profile,
+            prefix='AUTH',
+            severity='WARNING'
+            )
+        sys.exit(exit_codes['E_AUTHFAIL']['Code'])
+
+    failure = """ : Check of runtime parameters failed for unknown reason.
+    Please ensure local awscli is configured. Then run keyconfig to
+    configure keyup runtime parameters.   Exiting. Code: """
+    logger.warning(failure + 'Exit. Code: %s' % sys.exit(exit_codes['E_MISC']['Code']))
+    print(failure)
+
+
+if __name__ == '__main__':
+    init_cli()
