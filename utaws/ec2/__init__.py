52b97fc4b40bd35c727a62710ef17fc4746a7807
blakeca00 <blakeca00@gmail.com>
2018-05-13 11:14:19 -0500

    init module updates

--- utaws/ec2/__init__.py
+++ utaws/ec2/__init__.py
@@ -1,8 +1,5 @@
-from _version import __version__ as version
 
 __author__ = 'Blake Huber'
-__version__ = version
-__credits__ = []
 __license__ = "MIT"
 __maintainer__ = "Blake Huber"
 __email__ = "blakeca00[AT]gmail.com"
