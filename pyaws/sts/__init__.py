a5ee1849f6e110e4e1408844b0a74025534d6802
Blake Huber <blakeca00@gmail.com>
2018-05-21 06:22:56 -0500

    __init__.py updated for submodules

--- pyaws/sts/__init__.py
+++ pyaws/sts/__init__.py
@@ -1,9 +1,3 @@
-from _version import __version__ as version
-
-__author__ = 'Blake Huber'
-__version__ = version
-__credits__ = []
-__license__ = "MIT"
-__maintainer__ = "Blake Huber"
-__email__ = "blakeca00[AT]gmail.com"
-__status__ = "Development"
+"""
+Functionality utilising Amazon EC2 Service
+"""
