84554e5afcce5f6928270b3c14c0f7512d55f081
Blake Huber <blakeca00@gmail.com>
2018-05-21 05:05:58 -0500

    mv utaws >> pyaws

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """
 
-utaws :  Copyright 2017-2018, Blake Huber
+pyaws :  Copyright 2017-2018, Blake Huber
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -26,7 +26,7 @@ from setuptools.command.develop import develop
 from setuptools.command.install import install
 from subprocess import check_call
 from codecs import open
-import utaws
+import pyaws
 
 
 requires = [
@@ -69,13 +69,13 @@ class PostInstall(install):
 
 
 setup(
-    name='utaws',
-    version=utaws.__version__,
+    name='pyaws',
+    version=pyaws.__version__,
     description='Python Utilities for Amazon Web Services',
     long_description=read('DESCRIPTION.rst'),
-    url='http://utaws.readthedocs.io',
-    author=utaws.__author__,
-    author_email=utaws.__email__,
+    url='http://pyaws.readthedocs.io',
+    author=pyaws.__author__,
+    author_email=pyaws.__email__,
     license='GPL-3.0',
     classifiers=[
         'Topic :: System :: Systems Administration',
@@ -85,8 +85,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: Microsoft :: Windows'
+        'Operating System :: POSIX :: Linux'
     ],
     keywords='Amazon Web Services AWS iam ec2 lambda rds s3 sts',
     packages=find_packages(exclude=['docs', 'scripts', 'assets']),
@@ -94,8 +93,8 @@ setup(
     python_requires='>=3.4, <4',
     entry_points={
         'console_scripts': [
-            'utaws=utaws.cli:init_cli',
-            'utawsconfig=utaws.cli:option_configure'
+            'pyaws=pyaws.cli:init_cli',
+            'pyconfig=pyaws.cli:option_configure'
         ]
     },
     zip_safe=False
