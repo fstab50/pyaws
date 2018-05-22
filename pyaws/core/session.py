92ea333db4dedda88e2e5369a86a39c77b304d59
Blake Huber <blake.huber@atos.net>
2018-05-22 07:43:51 -0500

    pyaws/core/session.py

--- pyaws/core/session.py
+++ pyaws/core/session.py
@@ -18,39 +18,6 @@ DEFAULT_REGION = os.environ['AWS_DEFAULT_REGION']
 logger = loggers.getLogger(__version__)
 
 
-def authenticated(profile):
-    """
-    Summary:
-        Tests generic authentication status to AWS Account
-    Args:
-        :profile (str): iam user name from local awscli configuration
-    Returns:
-        TYPE: bool, True (Authenticated)| False (Unauthenticated)
-    """
-    try:
-        sts_client = boto3_session(service='sts', profile=profile)
-        httpstatus = sts_client.get_caller_identity()['ResponseMetadata']['HTTPStatusCode']
-        if httpstatus == 200:
-            return True
-
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'InvalidClientTokenId':
-            logger.info(
-                '%s: Invalid credentials to authenticate for profile user (%s). Exit. [Code: %d]'
-                % (inspect.stack()[0][3], profile, exit_codes['EX_NOPERM']['Code']))
-        elif e.response['Error']['Code'] == 'ExpiredToken':
-            logger.info(
-                '%s: Expired temporary credentials detected for profile user (%s) [Code: %d]'
-                % (inspect.stack()[0][3], profile, exit_codes['EX_CONFIG']['Code']))
-        else:
-            logger.exception(
-                '%s: Unknown Boto3 problem. Error: %s' %
-                (inspect.stack()[0][3], e.response['Error']['Message']))
-    except Exception as e:
-        return False
-    return False
-
-
 def boto3_session(service, region=DEFAULT_REGION, profile=None):
     """
     Summary:
@@ -84,3 +51,36 @@ def boto3_session(service, region=DEFAULT_REGION, profile=None):
         logger.warning(msg)
         sys.exit(exit_codes['EX_NOUSER']['Code'])
     return client
+
+
+def authenticated(profile):
+    """
+    Summary:
+        Tests generic authentication status to AWS Account
+    Args:
+        :profile (str): iam user name from local awscli configuration
+    Returns:
+        TYPE: bool, True (Authenticated)| False (Unauthenticated)
+    """
+    try:
+        sts_client = boto3_session(service='sts', profile=profile)
+        httpstatus = sts_client.get_caller_identity()['ResponseMetadata']['HTTPStatusCode']
+        if httpstatus == 200:
+            return True
+
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'InvalidClientTokenId':
+            logger.info(
+                '%s: Invalid credentials to authenticate for profile user (%s). Exit. [Code: %d]'
+                % (inspect.stack()[0][3], profile, exit_codes['EX_NOPERM']['Code']))
+        elif e.response['Error']['Code'] == 'ExpiredToken':
+            logger.info(
+                '%s: Expired temporary credentials detected for profile user (%s) [Code: %d]'
+                % (inspect.stack()[0][3], profile, exit_codes['EX_CONFIG']['Code']))
+        else:
+            logger.exception(
+                '%s: Unknown Boto3 problem. Error: %s' %
+                (inspect.stack()[0][3], e.response['Error']['Message']))
+    except Exception as e:
+        return False
+    return False
