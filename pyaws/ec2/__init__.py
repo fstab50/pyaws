490daea67a00419fbdace3f42d4ef63965d3deb9
Blake Huber <blakeca00@gmail.com>
2018-05-28 09:14:25 -0500

    __init__.py: test import configuration for ec2 module

--- pyaws/ec2/__init__.py
+++ pyaws/ec2/__init__.py
@@ -1,4 +1,6 @@
 """
 Functionality utilising Amazon EC2 Service
 """
-from pyaws.ec2 import *
+from pyaws.ec2.ec2_utils import *
+from pyaws.ec2.current_ami import *
+from pyaws.ec2.snapshot_ops import *
