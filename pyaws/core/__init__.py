e8d6f26114b6f330ab0bdf75f6cd5ef273c33a16
Blake Huber <blakeca00@gmail.com>
2018-06-26 14:02:50 -0500

    mv script_utils.py --> utils dir

--- pyaws/core/__init__.py
+++ pyaws/core/__init__.py
@@ -2,6 +2,5 @@
 Common AWS Functionality required to support all services
 """
 from pyaws.core.session import *
-from pyaws.core.script_utils import *
 from pyaws.core import loggers
 from pyaws.core import colors
