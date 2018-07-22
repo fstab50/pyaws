94760f0135b1d8b5226aa8602b631261f49633d0
Blake Huber <blake.huber@atos.net>
2018-07-22 11:55:16 -0500

    rm

--- pyaws/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Common AWS Functionality required to support all services
-"""
-from pyaws.utils.script_utils import *
-# should below be imported when utils imported? Exception
-#from pyaws.core import loggers
-#from pyaws.core import colors
