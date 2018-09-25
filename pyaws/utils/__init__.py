b6acaa8aef795c388fc210e6e00fdc41c0703469
blakeca00 <blakeca00@gmail.com>
2018-09-25 05:58:22 -0500

    pyaws/utils/__init__.py: added import for new utils.export module

--- pyaws/utils/__init__.py
+++ pyaws/utils/__init__.py
@@ -4,3 +4,4 @@ Utilities Module - Scripts
 from pyaws.utils.userinput import *
 from pyaws.utils.time import *
 from pyaws.utils.usermessage import stdout_message
+from pyaws.utils.export import export_json_object
