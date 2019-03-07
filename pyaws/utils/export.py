da8cf28d94dc28c7880b0e455e0ceed524bb8623
Blake Huber <blakeca00@gmail.com>
2019-03-07 05:14:13 -0600

    pyaws/utils/export.py: reformatting of entire module

--- pyaws/utils/export.py
+++ pyaws/utils/export.py
@@ -19,24 +19,39 @@ def export_json_object(dict_obj, filename=None, logging=True):
 
     """
     try:
+
         if filename:
+
             try:
+
                 with open(filename, 'w') as handle:
                     handle.write(json.dumps(dict_obj, indent=4, sort_keys=True))
                     logger.info(
                         '%s: Wrote %s to local filesystem location' %
                         (inspect.stack()[0][3], filename))
+
                 handle.close()
+
             except TypeError as e:
                 logger.warning(
                     '%s: object in dict not serializable: %s' %
                     (inspect.stack()[0][3], str(e)))
+
         else:
             json_str = json.dumps(dict_obj, indent=4, sort_keys=True)
-            print(highlight(json_str, lexers.JsonLexer(), formatters.TerminalFormatter()).strip())
+
+            print(
+                highlight(
+                    json_str,
+                    lexers.JsonLexer(),
+                    formatters.TerminalFormatter()
+                ).strip()
+            )
+
             if logging:
                 logger.info('%s: successful export to stdout' % inspect.stack()[0][3])
             return True
+
     except OSError as e:
         logger.critical(
             '%s: export_file_object: error writing to %s to filesystem. Error: %s' %
