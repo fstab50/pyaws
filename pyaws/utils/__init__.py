3e30249862bed1c1cd9a036f812e5b59f7fb328e
Blake Huber <blakeca00@gmail.com>
2019-07-01 12:56:44 -0500

    implement libtools, move all utils functionality external library

--- pyaws/utils/__init__.py
+++ pyaws/utils/__init__.py
@@ -2,10 +2,10 @@
 Utilities Module - Scripts
 """
 
-from pyaws.utils.userinput import bool_convert, bool_assignment, ascii_lowercase
-from pyaws.utils.userinput import range_test, range_bind, userchoice_mapping
-from pyaws.utils.time import *
-from pyaws.utils.usermessage import stdout_message
-from pyaws.utils.export import export_json_object
-from pyaws.utils.concurrency import split_list
-from pyaws.utils.progress import screen_dimensions, progress_meter
+from libtools.userinput import bool_convert, bool_assignment, ascii_lowercase
+from libtools.userinput import range_test, range_bind, userchoice_mapping
+from libtools.time import *
+from libtools.usermessage import stdout_message
+from libtools.export import export_json_object
+from libtools.concurrency import split_list
+from libtools.progress import screen_dimensions, progress_meter
